383f9f3e2ae820b36bba2a3e2f1f3120b9a6954d
Afiya Jamil <aj7046@srmist.edu.com>
2026-03-02 13:32:30 +0530

    use an array to print the banneroops

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,18 +1,19 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] lines = new String[7];
 
-        lines[0] = String.join("   ", "  *****   ", "  *****   ", "*******   ", "  *****  ");
+        String[] lines = {
 
-        lines[1] = String.join("   ", " *     *  ", " *     *  ", "*      *  ", " *     * ");
-        lines[2] = String.join("   ", "*       * ", "*       * ", "*      *  ", "*        ");
-        lines[3] = String.join("   ", "*       * ", "*       * ", "*******   ", "  *****  ");
-        lines[4] = String.join("   ", "*       * ", "*       * ", "*         ", "       * ");
-        lines[5] = String.join("   ", " *     *  ", " *     *  ", "*         ", " *     * ");
-        lines[6] = String.join("   ", "  *****   ", "  *****   ", "*         ", "  *****  ");
-
-        for (int i = 0; i < 6; i++) {
-            System.out.println(lines[i]);
-            }
+            String.join("   ", "  *****   ", "  *****   ", "*******   ", "  *****  "),
+            String.join("   ", " *     *  ", " *     *  ", "*      *  ", " *     * "),
+            String.join("   ", "*       * ", "*       * ", "*      *  ", "*        "),
+            String.join("   ", "*       * ", "*       * ", "*******   ", "  *****  "),
+            String.join("   ", "*       * ", "*       * ", "*         ", "       * "),
+        
+            String.join("   ", " *     *  ", " *     *  ", "*         ", " *     * "),
+            String.join("   ", "  *****   ", "  *****   ", "*         ", "  *****  "),
+        };
+       
+    for(String line : lines)
+        System.out.println(line);
     }
 }
